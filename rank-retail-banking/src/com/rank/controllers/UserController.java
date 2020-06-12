83d5200a1b0d4d8a79535225995897f956866713
ARUN BALA <arunbala7@users.noreply.github.com>
2020-06-12 15:05:21 +0530

    User Login Controller

--- rank-retail-banking/src/com/rank/controllers/UserController.java
+++ rank-retail-banking/src/com/rank/controllers/UserController.java
@@ -8,23 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class User
+ * User Login and Logout controller
  */
 @WebServlet("/User")
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
