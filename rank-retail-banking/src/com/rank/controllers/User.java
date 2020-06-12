8e6cc235d869c8733a4381a6b58b4ebe3cf5902b
ARUN BALA <arunbala7@users.noreply.github.com>
2020-06-12 14:45:10 +0530

    MVC Project Structure

--- rank-retail-banking/src/com/rank/controllers/User.java
+++ rank-retail-banking/src/com/rank/controllers/User.java
@@ -14,17 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 public class User extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public User() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
