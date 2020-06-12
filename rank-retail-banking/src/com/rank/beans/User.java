2c703d0fc620574b11312ab74eadb89f9741eb3b
ARUN BALA <arunbala7@users.noreply.github.com>
2020-06-12 13:28:30 +0530

    User Bean Class

--- rank-retail-banking/src/com/rank/beans/User.java
+++ rank-retail-banking/src/com/rank/beans/User.java
@@ -1,5 +1,28 @@
 package com.rank.beans;
 
 public class User {
+String userName,password;
+
+public User(String userName, String password) {
+	super();
+	this.userName = userName;
+	this.password = password;
+}
+
+public String getUserName() {
+	return userName;
+}
+
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
 
 }
